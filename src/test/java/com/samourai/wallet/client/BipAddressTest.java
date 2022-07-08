f397d22ca623f07fb673f5b6f2feb00a646b92d7
zeroleak <zeroleak@samourai.io>
2022-07-08 10:45:27 +0200

    fix tests

--- src/test/java/com/samourai/wallet/client/BipAddressTest.java
+++ src/test/java/com/samourai/wallet/client/BipAddressTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 public class BipAddressTest extends AbstractTest {
   private BipWallet bipWalletDeposit;
   private BipWallet bipWalletPremix;
+  private BipWallet bipWalletPostmix;
 
   public BipAddressTest() throws Exception {
     super();
@@ -23,6 +24,7 @@ public class BipAddressTest extends AbstractTest {
 
     bipWalletDeposit = walletSupplier.getWallet(BIP_WALLET.DEPOSIT_BIP44);
     bipWalletPremix = walletSupplier.getWallet(BIP_WALLET.PREMIX_BIP84);
+    bipWalletPostmix = walletSupplier.getWallet(BIP_WALLET.POSTMIX_BIP84);
   }
 
   @Test
@@ -35,11 +37,18 @@ public class BipAddressTest extends AbstractTest {
         "mvBRvDzdcwrAQ4rk6bQy3MMSz5J1GDJgDB", bipWalletDeposit.getAddressAt(1, 1).getAddressString());
 
     Assertions.assertEquals(
-            "tb1qef2tq6prhk2u0a09pgllm73m5vl4q5qtcu2fp3", bipWalletPremix.getAddressAt(0, 0).getAddressString());
+            "tb1qrhl7dcu4e9vj0e9pd68xh4ks2czygljdlpfkdm", bipWalletPremix.getAddressAt(0, 0).getAddressString());
     Assertions.assertEquals(
-            "tb1q6jfh0zp0mjfem9g655drczkkm49p2t3xlqyd69", bipWalletPremix.getAddressAt(1, 0).getAddressString());
+            "tb1qrd0gj9eqnpsqvwl2tr2jq08m0v0ujyh0r9fw7a", bipWalletPremix.getAddressAt(1, 0).getAddressString());
     Assertions.assertEquals(
-            "tb1qv7n3qjsn449nrm4q5hvlrpj6p2d077mmukjd3e", bipWalletPremix.getAddressAt(1, 1).getAddressString());
+            "tb1qhxwf7pj23nxwzfhusm9t0n26e3znj3sq0gu8w6", bipWalletPremix.getAddressAt(1, 1).getAddressString());
+
+    Assertions.assertEquals(
+            "tb1qef2tq6prhk2u0a09pgllm73m5vl4q5qtcu2fp3", bipWalletPostmix.getAddressAt(0, 0).getAddressString());
+    Assertions.assertEquals(
+            "tb1q6jfh0zp0mjfem9g655drczkkm49p2t3xlqyd69", bipWalletPostmix.getAddressAt(1, 0).getAddressString());
+    Assertions.assertEquals(
+            "tb1qv7n3qjsn449nrm4q5hvlrpj6p2d077mmukjd3e", bipWalletPostmix.getAddressAt(1, 1).getAddressString());
 
   }
 
@@ -53,10 +62,17 @@ public class BipAddressTest extends AbstractTest {
             "m/44'/1'/0'/1/1", bipWalletDeposit.getAddressAt(1, 1).getPathAddress());
 
     Assertions.assertEquals(
-            "m/84'/1'/2147483646'/0/0", bipWalletPremix.getAddressAt(0, 0).getPathAddress());
+            "m/84'/1'/2147483645'/0/0", bipWalletPremix.getAddressAt(0, 0).getPathAddress());
     Assertions.assertEquals(
-            "m/84'/1'/2147483646'/1/0", bipWalletPremix.getAddressAt(1, 0).getPathAddress());
+            "m/84'/1'/2147483645'/1/0", bipWalletPremix.getAddressAt(1, 0).getPathAddress());
     Assertions.assertEquals(
-            "m/84'/1'/2147483646'/1/1", bipWalletPremix.getAddressAt(1, 1).getPathAddress());
+            "m/84'/1'/2147483645'/1/1", bipWalletPremix.getAddressAt(1, 1).getPathAddress());
+
+    Assertions.assertEquals(
+            "m/84'/1'/2147483646'/0/0", bipWalletPostmix.getAddressAt(0, 0).getPathAddress());
+    Assertions.assertEquals(
+            "m/84'/1'/2147483646'/1/0", bipWalletPostmix.getAddressAt(1, 0).getPathAddress());
+    Assertions.assertEquals(
+            "m/84'/1'/2147483646'/1/1", bipWalletPostmix.getAddressAt(1, 1).getPathAddress());
   }
 }
