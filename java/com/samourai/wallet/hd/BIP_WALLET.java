24277ad876da8be41d6163c41032015fbf529678
zeroleak <zeroleak@samourai.io>
2022-08-20 06:05:20 +0200

    revert BipWallet changes related to POSTMIX_BIP84 and use POSTMIX_BIP84_AS_BIP49 + POSTMIX_BIP84_AS_BIP44

--- java/com/samourai/wallet/hd/BIP_WALLET.java
+++ java/com/samourai/wallet/hd/BIP_WALLET.java
@@ -1,16 +1,12 @@
 package com.samourai.wallet.hd;
 
 import com.samourai.wallet.bipFormat.BIP_FORMAT;
-import com.samourai.wallet.bipWallet.BipDerivation;
 import com.samourai.wallet.bipFormat.BipFormat;
+import com.samourai.wallet.bipWallet.BipDerivation;
 import com.samourai.whirlpool.client.wallet.beans.SamouraiAccountIndex;
 import com.samourai.whirlpool.client.wallet.beans.WhirlpoolAccount;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public enum BIP_WALLET {
 
@@ -18,7 +14,9 @@ public enum BIP_WALLET {
   DEPOSIT_BIP49(WhirlpoolAccount.DEPOSIT, new BipDerivation(Purpose.PURPOSE_49, SamouraiAccountIndex.DEPOSIT), BIP_FORMAT.SEGWIT_COMPAT),
   DEPOSIT_BIP84(WhirlpoolAccount.DEPOSIT, new BipDerivation(Purpose.PURPOSE_84, SamouraiAccountIndex.DEPOSIT), BIP_FORMAT.SEGWIT_NATIVE),
   PREMIX_BIP84(WhirlpoolAccount.PREMIX, new BipDerivation(Purpose.PURPOSE_84, SamouraiAccountIndex.PREMIX), BIP_FORMAT.SEGWIT_NATIVE),
-  POSTMIX_BIP84(WhirlpoolAccount.POSTMIX, new BipDerivation(Purpose.PURPOSE_84, SamouraiAccountIndex.POSTMIX), List.of(BIP_FORMAT.SEGWIT_NATIVE, BIP_FORMAT.SEGWIT_COMPAT, BIP_FORMAT.LEGACY)),
+  POSTMIX_BIP84(WhirlpoolAccount.POSTMIX, new BipDerivation(Purpose.PURPOSE_84, SamouraiAccountIndex.POSTMIX), BIP_FORMAT.SEGWIT_NATIVE),
+  POSTMIX_BIP84_AS_BIP49(WhirlpoolAccount.POSTMIX, new BipDerivation(Purpose.PURPOSE_84, SamouraiAccountIndex.POSTMIX), BIP_FORMAT.SEGWIT_COMPAT),
+  POSTMIX_BIP84_AS_BIP44(WhirlpoolAccount.POSTMIX, new BipDerivation(Purpose.PURPOSE_84, SamouraiAccountIndex.POSTMIX), BIP_FORMAT.LEGACY),
 
   BADBANK_BIP84(WhirlpoolAccount.BADBANK, new BipDerivation(Purpose.PURPOSE_84, SamouraiAccountIndex.BADBANK), BIP_FORMAT.SEGWIT_NATIVE),
   RICOCHET_BIP84(WhirlpoolAccount.RICOCHET, new BipDerivation(Purpose.PURPOSE_84, SamouraiAccountIndex.RICOCHET), BIP_FORMAT.SEGWIT_NATIVE);
@@ -26,18 +24,12 @@ public enum BIP_WALLET {
   private static final Logger log = LoggerFactory.getLogger(BIP_WALLET.class);
   private WhirlpoolAccount account;
   private BipDerivation bipDerivation;
-  private List<BipFormat> bipFormats;
+  private BipFormat bipFormat;
 
   BIP_WALLET(WhirlpoolAccount account, BipDerivation bipDerivation, BipFormat bipFormat) {
     this.account = account;
     this.bipDerivation = bipDerivation;
-    this.bipFormats = new ArrayList<>(Collections.singleton(bipFormat));
-  }
-
-  BIP_WALLET(WhirlpoolAccount account, BipDerivation bipDerivation, List<BipFormat> bipFormats) {
-    this.account = account;
-    this.bipDerivation = bipDerivation;
-    this.bipFormats = bipFormats;
+    this.bipFormat = bipFormat;
   }
 
   public WhirlpoolAccount getAccount() {
@@ -48,7 +40,7 @@ public enum BIP_WALLET {
     return bipDerivation;
   }
 
-  public List<BipFormat> getBipFormats() {
-    return bipFormats;
+  public BipFormat getBipFormat() {
+    return bipFormat;
   }
 }
