c307412c4b3ed9dc6ad3ea2285444b177a4c7d4a
zeroleak <zeroleak@samourai.io>
2023-05-17 06:14:16 +0200

    add WhirlpoolServer.signingPaymentCode

--- java/com/samourai/whirlpool/client/wallet/beans/WhirlpoolServer.java
+++ java/com/samourai/whirlpool/client/wallet/beans/WhirlpoolServer.java
@@ -1,6 +1,7 @@
 package com.samourai.whirlpool.client.wallet.beans;
 
 import com.samourai.dex.config.DexConfigProvider;
+import com.samourai.wallet.bip47.rpc.PaymentCode;
 import com.samourai.wallet.util.FormatsUtilGeneric;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.params.MainNetParams;
@@ -13,34 +14,41 @@ public enum WhirlpoolServer {
           DexConfigProvider.getInstance().getSamouraiConfig().getWhirlpoolServerTestnetClear(),
           DexConfigProvider.getInstance().getSamouraiConfig().getWhirlpoolServerTestnetOnion(),
           TestNet3Params.get(),
-          "mi42XN9J3eLdZae4tjQnJnVkCcNDRuAtz4"),
+          "mi42XN9J3eLdZae4tjQnJnVkCcNDRuAtz4",
+          "PM8TJPTDuJei5ML1FinhAKuwHHfdBYq66zTdcqM2RMqAywefDeH6h5YAFP2hZDAdSkx2kK1s2XFnixQBPYw2YWDMdz4vbcePFGfNNVGLkyKFAYKcrqvZ"), // TODO
   INTEGRATION(
           DexConfigProvider.getInstance().getSamouraiConfig().getWhirlpoolServerIntegrationClear(),
           DexConfigProvider.getInstance().getSamouraiConfig().getWhirlpoolServerIntegrationOnion(),
           TestNet3Params.get(),
-          "mi42XN9J3eLdZae4tjQnJnVkCcNDRuAtz4"),
+          "mi42XN9J3eLdZae4tjQnJnVkCcNDRuAtz4",
+          "PM8TJPTDuJei5ML1FinhAKuwHHfdBYq66zTdcqM2RMqAywefDeH6h5YAFP2hZDAdSkx2kK1s2XFnixQBPYw2YWDMdz4vbcePFGfNNVGLkyKFAYKcrqvZ"), // TODO
   MAINNET(
           DexConfigProvider.getInstance().getSamouraiConfig().getWhirlpoolServerMainnetClear(),
           DexConfigProvider.getInstance().getSamouraiConfig().getWhirlpoolServerMainnetOnion(),
           MainNetParams.get(),
-          "1LYKqgLMJagcQ69Y9iLj4TNs4uH9surJ3w"),
+          "1LYKqgLMJagcQ69Y9iLj4TNs4uH9surJ3w",
+          "PM8TJPTDuJei5ML1FinhAKuwHHfdBYq66zTdcqM2RMqAywefDeH6h5YAFP2hZDAdSkx2kK1s2XFnixQBPYw2YWDMdz4vbcePFGfNNVGLkyKFAYKcrqvZ"), // TODO
   LOCAL_TESTNET("http://127.0.0.1:8080", "http://127.0.0.1:8080", TestNet3Params.get(),
-          "mi42XN9J3eLdZae4tjQnJnVkCcNDRuAtz4");
+          "mi42XN9J3eLdZae4tjQnJnVkCcNDRuAtz4",
+          "PM8TJPTDuJei5ML1FinhAKuwHHfdBYq66zTdcqM2RMqAywefDeH6h5YAFP2hZDAdSkx2kK1s2XFnixQBPYw2YWDMdz4vbcePFGfNNVGLkyKFAYKcrqvZ"); // TODO
 
   private String serverUrlClear;
   private String serverUrlOnion;
   private NetworkParameters params;
   private String signingAddress;
+  private PaymentCode signingPaymentCode;
 
   WhirlpoolServer(
           String serverUrlClear,
           String serverUrlOnion,
           NetworkParameters params,
-          String signingAddress) {
+          String signingAddress,
+          String signingPaymentCode) {
     this.serverUrlClear = serverUrlClear;
     this.serverUrlOnion = serverUrlOnion;
     this.params = params;
     this.signingAddress = signingAddress;
+    this.signingPaymentCode = new PaymentCode(signingPaymentCode);
   }
 
   public String getServerUrlClear() {
@@ -62,6 +70,10 @@ public enum WhirlpoolServer {
 
   public String getSigningAddress() {
     return signingAddress;
+  }
+
+  public PaymentCode getSigningPaymentCode() {
+    return signingPaymentCode;
   }
 
   public static Optional<WhirlpoolServer> find(String value) {
