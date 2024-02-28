b61e95270696e50516e85a5123fa4aec4d935bb2
zeroleak <zeroleak@samourai.io>
2024-02-28 07:23:28 +0100

    refactor: add HttpProxy, IHttpProxyService

--- java/com/samourai/wallet/httpClient/IHttpClient.java
+++ java/com/samourai/wallet/httpClient/IHttpClient.java
@@ -11,4 +11,5 @@ public interface IHttpClient extends IBackendClient {
 
   <T> Single<Optional<T>> postJson(
           String url, Class<T> responseType, Map<String, String> headers, Object body);
+  Single<Optional<String>> postString(String urlStr, Map<String, String> headers, String contentType, String content);
 }
