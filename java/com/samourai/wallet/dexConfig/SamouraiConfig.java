a46b4a83f770b1c6fd5f42b9f5880717c76ca4e4
zeroleak <zeroleak@samourai.io>
2024-03-20 14:19:56 +0100

    refactor: upgrade soroban mainnet cluster

--- java/com/samourai/wallet/dexConfig/SamouraiConfig.java
+++ java/com/samourai/wallet/dexConfig/SamouraiConfig.java
@@ -17,7 +17,7 @@ public class SamouraiConfig {
     private String sorobanServerMainnetOnion = "http://sorob4sg7yiopktgz4eom7hl5mcodr6quvhmdpljl5qqhmt6po7oebid.onion";
 
     // extlibj: SorobanServerDex
-    private Collection<String> sorobanServerDexTestnetClear = Arrays.asList( // TODO test cluster
+    private Collection<String> sorobanServerDexTestnetClear = Arrays.asList(
             "http://163.172.128.201:4242",
             "http://163.172.130.151:4242",
             "http://163.172.159.127:4242",
@@ -40,37 +40,26 @@ public class SamouraiConfig {
             "http://sorbutari3lpxmqhpzu3jojflteluekxnfyu2jgs4vqkgtsxrjyv4byd.onion"
     );
     private Collection<String> sorobanServerDexMainnetClear = Arrays.asList(
-            "http://163.172.128.201:4242",
-            "http://163.172.130.151:4242",
-            "http://163.172.159.127:4242",
-            "http://163.172.174.29:4242",
-            "http://163.172.159.227:4242",
-            "http://51.15.192.136:4242",
-            "http://51.158.116.168:4242",
-            "http://51.15.226.163:4242",
-            "http://212.47.230.157:4242"
+            "http://185.100.84.98:4242",
+            "http://185.100.84.130:4242",
+            "http://185.100.84.131:4242",
+            "http://185.100.84.132:4242",
+            "http://185.100.84.133:4242",
+            "http://185.100.84.134:4242",
+            "http://185.100.84.135:4242",
+            "http://185.100.84.136:4242",
+            "http://185.100.84.137:4242"
     );
     private Collection<String> sorobanServerDexMainnetOnion = Arrays.asList(
-            "http://sorin6xws7lfvz2ikma3ceqzlbmmp4huyfwla3d5mzkcjnta2djgggid.onion",
-            "http://sor2aduqon52pngz56b3pp2niq5vp4l7xstco654gfa37fcaoblle5yd.onion",
-            "http://sor3xcg6i4tyt4uawt2t3i3ml4dzubh46wyloptl3g5jyshkgekuumyd.onion",
-            "http://sor4ky4w6iywr3terpofu3xlnwrnawbb4aq2okobixojatswelfpsuad.onion",
-            "http://sor5qv4q3uvg3cyfaay5dazkvuytukepexlqehgirxbq7ynszpqo4uad.onion",
-            "http://sor6jatlc2pim7mg4paxy6kgzuw7qidajlxk7xy6ic6ytcpcy47lucyd.onion",
-            "http://sor7qfbf24l3gdba5ed625pfwfebwctiao5po3zux3c6udlboowkucid.onion",
-            "http://sorark2anb6q6oz6egxo4zo67cmlnfvjxz2h34v74ta6jozceqclw5yd.onion",
-            "http://sorbutari3lpxmqhpzu3jojflteluekxnfyu2jgs4vqkgtsxrjyv4byd.onion",
-
-            // onion only
-            "http://g35mf2l5zzbzldqfq5sfdsw3fibajh5rytnppyb634gn6b5o3fhif6yd.onion",
-            "http://3y3j4dgul5767ibxkll3siadcojmhqudeaz7f2i3grsgmklkpntrbbyd.onion",
-            "http://jvdzspo3kkhztcyoddcgednw6643zuvfubha43bj4zh7jqre27lgvvad.onion",
-            "http://so4cne7cfg5tbqe6d5c2q5ci2ezpg7ii4vm2w5cu7v37bvyjfv6g6fid.onion",
-            "http://7m4eaaxxpbwnoy77qx6nfxqjgbe3ttbkb6f4lftvohzia2ph5qnx2vad.onion",
-            "http://lyxiuvhkbvrp2krprwlj5z5oip52qlle6woryty6fhs4r7vqxrnkupad.onion",
-            "http://thcywo6avdbjwamyt2vilfw7kb254a7adb42nud63qfvpysdetxzphqd.onion",
-            "http://v5uevs57okmzsrz5bdald2qvs7k3m3ekotujjvpnaqiuwnuafbnhadqd.onion",
-            "http://445hebzvpkm2gwohxxzwohdw5wnxwg5lth67nakmg7fmdqbcry4irvqd.onion"
+            "http://wmk44xtgyfmc2noamkeoetnc7ufmkqso4apfnnvp4or4mibscumr22id.onion",
+            "http://hp4gpnhxkx6u7wzc3fkqf263ni4zfpcta4uxi24bcr2bzfcnu2lw5did.onion",
+            "http://ubbd4kioblwsvmpj3hhwvi7vfidzx4why2zui4n5cttuajxxfvzwbkqd.onion",
+            "http://6capyi2cfpa7ofpgqb6vippiqw4kwvo7qo3pqzf7xem5uso6uvu7ezyd.onion",
+            "http://ayhsx4zxjq4tsk3gkwbppq2u2d6wllfsvubbaro45up53zgmqp2ghcqd.onion",
+            "http://idmxhjkai4ptjflenq37vsaz4jvyk5pgg2hpmyjefeflhb23g5yej5yd.onion",
+            "http://dncxro3yxldpyznkkf7rxkthh4qf2vw26uzxbfcab34wrgaqve76yyid.onion",
+            "http://oxtyspz3im74ebdaxq3ozm2y6jlgiru4m6wn7oznvc6nwpiububel7ad.onion",
+            "http://334cb6simoua5uiy4buacqxuh56u3mukeggu3p3xnlmyp7skirv2jzid.onion"
     );
 
     // whrilpool-client: WhrilpoolServer
